2170c4e974fe2ef24e8cca175361e092a320e990
Alistair Kane <vplelmo@hotmail.co.uk>
2017-12-04 16:42:59 +0000

    removes original cat HTML & adds them to the catArray

--- public/app.js
+++ public/app.js
@@ -70,12 +70,14 @@ var app = function(){
             createCatObject(cat);
         }
     }
-
-    var Whiskers = new Cat("https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg", "Arrays", "CodeCat")
+    var Boba = new Cat("http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg", "Sock fluff", "Boba");
+    var Barnaby = new Cat("https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg", "Tuna", "Barnaby");
+    var Max = new Cat("http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg", "Whiskas Temptations", "Max");
+    var CodeCat = new Cat("https://mymodernmet.com/wp/wp-content/uploads/2017/11/chimera-cat-quimera-feat-sm.jpg", "Arrays", "CodeCat");
     var Grumpers = new Cat("http://i0.kym-cdn.com/entries/icons/facebook/000/011/365/GRUMPYCAT.jpg", "Choc ices", "Grumpers");
     var Flanders = new Cat("https://cdn.theatlantic.com/assets/media/img/mt/2017/08/GettyImages_161785570/lead_960.jpg?1502385684", "Hash Browns", "Flanders");
 
-    var ArrayOfCats = [Whiskers, Grumpers, Flanders];
+    var ArrayOfCats = [Boba, Barnaby, Max, CodeCat, Grumpers, Flanders];
 
     // createCatObject(Whiskers);
     // createACat("https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg", "Abandoned Mice", "Bobby");
